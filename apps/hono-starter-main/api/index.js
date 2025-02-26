7d403ef3def18caba165199762e9069ae3788634
Kyle McGinn <kyle.mcginn@turn2sc.com>
2025-02-26 10:44:16 -0600

    Added a math package

--- apps/hono-starter-main/api/index.js
+++ apps/hono-starter-main/api/index.js
@@ -1,11 +1,13 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { add } from "@repo/math/add";
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath("/api");
 
-app.get('/', (c) => {
-  return c.json({ message: "Congrats! You've deployed Hono to Vercel" })
-})
+app.get("/", (c) => {
+  const sum = add(1, 3);
+  return c.json({ message: "Congrats! You've deployed Hono to Vercel", sum });
+});
 
 const handler = handle(app);
 
